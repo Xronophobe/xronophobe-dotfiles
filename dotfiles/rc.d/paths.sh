e050e5bdf0bb87fe3fa82d3cf3e7e580a6437e3f
Csanad Beres <qed.csberes@gmail.com>
2017-08-20 12:02:11 +0200

    add BOOST, ZSH paths

--- dotfiles/rc.d/paths.sh
+++ dotfiles/rc.d/paths.sh
@@ -1,1 +1,6 @@
-export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/local/bin:/opt/local/sbin"
+export BOOST_ROOT="$HOME/opt/boost_1_64_0"
+
+# extensions
+
+export PATH="$PATH:/home/xphe/opt/boost/bin"
